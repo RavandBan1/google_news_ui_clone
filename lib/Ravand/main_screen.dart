3b2f0b0f690a9cd2d5510e28a5b69c66e55170f2
ravandban1 <ravandb4n1@gmail.com>
2023-02-16 14:01:10 +0300

    -Added bottomNav file with implementation. -Added appBar to main_screen file. -Added Dark theme to main file. -Added Google News app Icon and Label for Android.

--- lib/Ravand/main_screen.dart
+++ lib/Ravand/main_screen.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+class Foryou extends StatelessWidget {
+  const Foryou({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: const Text("Google News"),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          //for the Search icon button on top left of the screen
+          icon: const Icon(
+            Icons.search,
+            color: Colors.grey,
+          ),
+          tooltip: 'Search',
+          onPressed: () {}, //implementation later (TO DO)!!
+        ),
+        actions: <Widget>[
+          //for the top right gmail icon
+          IconButton(
+            icon: const Icon(Icons.email_outlined),
+            tooltip: 'Gmail',
+            onPressed: () {}, //implementation later (TO DO)!!
+          ),
+        ],
+      ),
+      //body implementation later
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_news_ui_clone/bottomNav.dart';
 
-void main() => runApp(const MaterialApp(
+void main() => runApp(MaterialApp(
+      color: Colors.grey[900],
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      theme: ThemeData.dark(),
+      home: const bottomNav(),
     ));
